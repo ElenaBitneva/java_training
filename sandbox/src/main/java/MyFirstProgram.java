1382e886a55e5d433abb978a8076bdf1ce759db3
ElenaBitneva <elina_336@mail.ru>
2018-03-10 18:47:59 -0500

    Инициализация точек реализована через конструктор

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -2,13 +2,9 @@ public class MyFirstProgram {
 
    public static void main (String[] args) {
  /* Создаем объекты, которые являются представителями класса Point, тоесть эта функция принимает объект типа Point */
-      Point p1 = new Point();
-      Point p2 = new Point();
-     /* Присваиваю точкам р1 и р2 атрибуты*/
-      p1.x = 10;
-      p1.y = 15;
-      p2.x = 18;
-      p2.y = 12;
+      Point p1 = new Point(10, 15);
+      Point p2 = new Point(18, 12);
+
 
 
       System.out.println("Дистанция между точками р1 и р2 = " + distance(p1, p2));
