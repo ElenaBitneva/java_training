949e52109d562a54765b66f5f01f7961bc5170f1
ElenaBitneva <elina_336@mail.ru>
2018-03-18 20:01:54 -0400

    Создан GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/elena/app1/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package elena.app1.addressbook.tests;
+
+import elena.app1.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+/**
+ * Created by elina_000 on 18.03.2018.
+ */
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getNavigationHelper().selectListElement();
+        app.getGroupHelper().intGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
